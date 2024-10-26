f79393750d64fee66322ebfba5f81afc425e11de
Dominique Morem <domi_castro@hotmail.com>
2024-10-25 22:00:04 -0300

    Segundo desafio do curso Java Spring professional da DevSuperior

---
+++ src/test/java/com/devsuperiorchallengetwo/challengeTwo/ChallengeTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorchallengetwo.challengeTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
